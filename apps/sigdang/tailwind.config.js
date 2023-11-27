8aa399c02b0d74c30267306151a2c0d15a2adcb7
vishalRK <vishalkerlekar5@gmail.com>
2023-11-27 09:09:13 +0530

    work in progress

--- apps/sigdang/tailwind.config.js
+++ apps/sigdang/tailwind.config.js
@@ -14,12 +14,27 @@ module.exports = {
     extend: {
       animation: {
         wiggle: 'wiggle 2s ease-in-out',
+        menuopen:'menu_open 5s ease-in-out alternate',
+        menuclose:'menu_close 5s ease-in-out reverse',
       },
+      
       keyframes: {
         wiggle: {
-          '0%': { transform: 'translateY(-20px)' ,opacity:0},
-          '100%': { transform: 'translateY(5px)',opacity:1 },
+          '0%': { transform: 'translateY(-5px)' ,opacity:0},
+          '100%': { transform: 'translateY(30px)',opacity:1 },
+          '0%': { transform: 'translateY(-180px)',opacity:0 },
+          
         },
+        menu_open: {
+          '0%': { transform: 'translateX(-15px)' ,left:"-200px"},
+          '100%': { transform: 'translateX(0px)',left:"0px" },
+        },
+        menu_close: {
+          '100%': { transform: 'translateX(0px)' ,left:"0px"},
+          '0%': { transform: 'translateX(-20px)',left:"-200px" ,width: '0px' ,visibility: 'hidden'},
+          // '0%':{visibility:""}
+        },
+        
       },
       screens: {
         small: { raw: '(max-width: 540px)' },
